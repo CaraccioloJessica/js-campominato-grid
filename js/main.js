5816dc0c835158835cc83fe1aa33a2d224c6ab60
Jessica <jesscara.993@gmail.com>
2022-11-16 14:31:48 +0100

    loop for creations of 100 div

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
 // L’utente clicca su un bottone che genererà una griglia di gioco quadrata. Ogni cella ha un numero progressivo, da 1 a 100. Ci saranno quindi 10 caselle per ognuna delle 10 righe. Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
+
+// VARIABILE CONTAINER
+const contenitore = document.getElementById('container');
+// console.log(contenitore);
+
+// LOOP PER CREARE 100 CASELLE
+for (let i = 0; i < 100; i++){
+  
+}
